34a7fe351d4ca9511630721508c104cb3f4b88c7
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-24 18:56:03 +0800

    Create cleanup_and_aligment.bash

---
+++ cleanup_and_aligment.bash
@@ -0,0 +1,29 @@
+#############################################################################################################
+# Adapter removel
+# "AGATCGGAAGAGC" adapter sequence
+trim_galore --illumina -o /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/ \
+/home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/RMH3993/RMH3993_MS001-SR-R00548-NoIndex_S1_L001_R1_001.fastq.gz
+
+trim_galore --illumina -o /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/ \
+/home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/RMH3994/RMH3994_MS001-SR-R00549-NoIndex_S1_L001_R1_001.fastq.gz
+#############################################################################################################
+# Genome index Preparation
+STAR --runThreadN 5 --runMode genomeGenerate --genomeDir \
+/logical_dev/resources/star_hg38_noAnnotation \
+--genomeFastaFiles /logical_dev/resources/hg38_allchr.fasta
+#############################################################################################################
+# Aligment
+STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+--readFilesCommand zcat --runThreadN 12 \
+--outFilterMismatchNoverLmax 0.05 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
+--outFilterMatchNminOverLread 0 --alignIntronMax 1 --alignEndsType EndToEnd \
+--readFilesIn /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/RMH3993_MS001-SR-R00548-NoIndex_S1_L001_R1_001_trimmed.fq.gz \
+ --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/bam/RMH3993_ 
+
+STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+--readFilesCommand zcat --runThreadN 12 \
+--outFilterMismatchNoverLmax 0.05 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
+--outFilterMatchNminOverLread 0 --alignIntronMax 1 --alignEndsType EndToEnd \
+--readFilesIn /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/RMH3994_MS001-SR-R00549-NoIndex_S1_L001_R1_001_trimmed.fq.gz \
+ --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/bam/RMH3994_ 
+###############################################################################################################
