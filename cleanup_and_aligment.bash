5e9aa03faf2fb6adfe7e41a508dbbdd02d9f4ced
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-26 00:51:45 +0800

    Update cleanup_and_aligment.bash

--- cleanup_and_aligment.bash
+++ cleanup_and_aligment.bash
@@ -21,14 +21,14 @@ STAR --runThreadN 14 --runMode genomeGenerate --genomeDir \
 --genomeFastaFiles /logical_dev/resources/mm10.fasta
 #############################################################################################################
 # Aligment
-STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+STAR --genomeDir /logical_dev/resources/star_mm10_noanno/ \
 --readFilesCommand zcat --runThreadN 12 \
 --outFilterMismatchNoverLmax 0.05 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
 --outFilterMatchNminOverLread 0 --alignIntronMax 1 --alignEndsType EndToEnd \
 --readFilesIn /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/RMH3993_MS001-SR-R00548-NoIndex_S1_L001_R1_001_trimmed.fq.gz \
  --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/bam/RMH3993_ 
 
-STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+STAR --genomeDir /logical_dev/resources/star_mm10_noanno/ \
 --readFilesCommand zcat --runThreadN 12 \
 --outFilterMismatchNoverLmax 0.05 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
 --outFilterMatchNminOverLread 0 --alignIntronMax 1 --alignEndsType EndToEnd \
@@ -36,14 +36,14 @@ STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
  --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/bam/RMH3994_ 
 ###############################################################################################################
 # Aligment 10% MM noE2E
-STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+STAR --genomeDir /logical_dev/resources/star_mm10_noanno/ \
 --readFilesCommand zcat --runThreadN 12 \
 --outFilterMismatchNoverLmax 0.10 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
 --outFilterMatchNminOverLread 0 --alignIntronMax 1 \
 --readFilesIn /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/RMH3993_MS001-SR-R00548-NoIndex_S1_L001_R1_001_trimmed.fq.gz \
  --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/bam/RMH3993_10MM_ 
 
-STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+STAR --genomeDir /logical_dev/resources/star_mm10_noanno/ \
 --readFilesCommand zcat --runThreadN 12 \
 --outFilterMismatchNoverLmax 0.10 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
 --outFilterMatchNminOverLread 0 --alignIntronMax 1 \
@@ -52,14 +52,14 @@ STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
 ###############################################################################################################
 
 # Aligment 10% MM E2E
-STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+STAR --genomeDir /logical_dev/resources/star_mm10_noanno/ \
 --readFilesCommand zcat --runThreadN 12 \
 --outFilterMismatchNoverLmax 0.10 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
 --outFilterMatchNminOverLread 0 --alignIntronMax 1  --alignEndsType EndToEnd \
 --readFilesIn /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/RMH3993_MS001-SR-R00548-NoIndex_S1_L001_R1_001_trimmed.fq.gz \
  --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/bam/RMH3993_10MM_e2e_ 
 
-STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+STAR --genomeDir /logical_dev/resources/star_mm10_noanno/ \
 --readFilesCommand zcat --runThreadN 12 \
 --outFilterMismatchNoverLmax 0.10 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
 --outFilterMatchNminOverLread 0 --alignIntronMax 1  --alignEndsType EndToEnd \
