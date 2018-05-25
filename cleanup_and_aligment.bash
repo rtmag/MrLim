a8714e8e9a4a45f77e6fc38114edb39b4d9437e7
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-05-25 11:52:42 +0800

    Update cleanup_and_aligment.bash

--- cleanup_and_aligment.bash
+++ cleanup_and_aligment.bash
@@ -27,3 +27,18 @@ STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
 --readFilesIn /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/RMH3994_MS001-SR-R00549-NoIndex_S1_L001_R1_001_trimmed.fq.gz \
  --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/bam/RMH3994_ 
 ###############################################################################################################
+# Aligment 10% MM noE2E
+STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+--readFilesCommand zcat --runThreadN 12 \
+--outFilterMismatchNoverLmax 0.10 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
+--outFilterMatchNminOverLread 0 --alignIntronMax 1 \
+--readFilesIn /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/RMH3993_MS001-SR-R00548-NoIndex_S1_L001_R1_001_trimmed.fq.gz \
+ --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/bam/RMH3993_10MM_ 
+
+STAR --genomeDir /logical_dev/resources/hg38_noanno/ \
+--readFilesCommand zcat --runThreadN 12 \
+--outFilterMismatchNoverLmax 0.10 --outFilterMatchNmin 16 --outFilterScoreMinOverLread 0 \
+--outFilterMatchNminOverLread 0 --alignIntronMax 1 \
+--readFilesIn /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/trimmed/RMH3994_MS001-SR-R00549-NoIndex_S1_L001_R1_001_trimmed.fq.gz \
+ --outSAMtype BAM SortedByCoordinate --outFileNamePrefix /home/rtm/CSI/mrLim/2018_05_17_13_50_30_833/bam/RMH3994_10MM_ 
+###############################################################################################################
